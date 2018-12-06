281c9f3ef7334a0ca932a7665e9cfbed51af3ae3
Alyssa Kennedy <alyssadrandall@gmail.com>
2018-12-06 19:55:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  
+  attr_accessor :item, :quanity, :price 
+  
+  # @@items = {}
+  
+  def initialize
+    @total = 0
+    @disc = 0
+  
+   end 
+   
+  # def discount(disc) 
+  #   self.total = self.total * disc 
+  # end 
+     
+     
+  # def add_item 
+    
+    
+ 
 
+end
